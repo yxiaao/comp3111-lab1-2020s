dc05acc59d7b815d56aa40215004044a23fd39cb
yxiaao <xiayunlei2000@outlook.com>
2020-12-30 10:53:14 +0800

    add a new output

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -6,6 +6,7 @@ public class Library {
 		Library lib = new Library();
 		if (lib.someLibraryMethod()) {
 			System.out.println("When there is a fire, commit and push");
+			System.out.println("hey")
 		}
 		
 	}
